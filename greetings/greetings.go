33d09525bd43b5ec97fb3235af2ce115707aafe2
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2025-07-12 21:42:45 -0300

    Lição de error handling em greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Hello retorna uma saudação para cada pessoa nomeada
 
-func Hello(name string) string {
+func Hello(name string) (string,error) {
 	// Retorna um nome interpolado com uma string
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Opa!, Tudo certo, %v?", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Printa a mensagem de saudação
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	
+	// Chama a mensagem de greetings 
+	message,err := greetings.Hello("")
+	// Se retorna um erro, printa no console  e exit o programa
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Se nenhum erro for retornado, printa a mensagem de greeting
 	fmt.Println(message)
 }
