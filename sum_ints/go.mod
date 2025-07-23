e40692b50d886308d080a1084c01819ab32db7e9
gustavo-silva98 <silva.gustavo98@gmail.com>
2025-07-23 00:50:06 -0300

    Testa deploy na lambda aws

--- sum_ints/go.mod
+++ sum_ints/go.mod
@@ -1,3 +1,5 @@
 module sum_ints
 
 go 1.24.4
+
+require github.com/aws/aws-lambda-go v1.49.0
--- sum_ints/main.go
+++ sum_ints/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Order struct {
+	OrderID string  `json:"order_id"`
+	Amount  float64 `json:"amount"`
+	Item    string  `json:"item"`
+}
 
 func main() {
-	fmt.Println(sum_ints(3, 3))
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(ctx context.Context, event json.RawMessage) (int, error) {
+	var order Order
+	if err := json.Unmarshal(event, &order); err != nil {
+		log.Printf("Failed to unmarshal event: %v", err)
+		return 0, err
+	}
+	return sum_ints(3, 3), nil
 }
 
 func sum_ints(a int, b int) int {
